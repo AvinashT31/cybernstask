00e7019869064161be353cf13c5b8da0d0408650
personal <thoratavinash446@gmail.com>
2024-11-05 16:00:19 +0530

    code

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import localFont from "next/font/local";
+import { Providers } from "./providers";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+          <Providers>
+            {children}
+          </Providers>
+      
+      </body>
+    </html>
+  );
+}
